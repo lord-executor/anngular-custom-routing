68039490433e495353ab23eb408dc67d9f0da2b3
Lukas Angerer <executor.dev@gmail.com>
2023-08-06 13:51:26 +0200

    Nested activation

--- src/app/routing-context.ts
+++ src/app/routing-context.ts
@@ -6,17 +6,38 @@ import { Route } from "./route";
 export class RoutingContext {
   private readonly outlets: Map<number, RouterOutletComponent> = new Map<number, RouterOutletComponent>();
 
+  private activeRoute: Array<Route> | null = null;
+  private awaitingActivation: number = -1;
+
   public registerOutlet(level: number, outlet: RouterOutletComponent): () => void {
     this.outlets.set(level, outlet);
+
+    if (this.awaitingActivation >= 0 && this.activeRoute) {
+      this.outlets.get(this.awaitingActivation)!.activate(this.activeRoute[this.awaitingActivation].component);
+      this.awaitingActivation++;
+      if (this.awaitingActivation >= this.activeRoute.length) {
+        this.awaitingActivation = -1;
+        console.log('fully activated route', this.activeRoute);
+      }
+    }
+
     return () => {
       this.outlets.delete(level);
     }
   }
 
   public activate(route: Array<Route>): void {
-    let level = 0;
-    if (this.outlets.has(level)) {
-      this.outlets.get(level)!.activate(route[level].component);
+    this.activeRoute = route;
+
+    this.awaitingActivation = 0;
+    while (this.awaitingActivation < this.activeRoute.length)
+    {
+      if (this.outlets.has(this.awaitingActivation)) {
+        this.outlets.get(this.awaitingActivation)!.activate(this.activeRoute[this.awaitingActivation].component);
+        this.awaitingActivation++;
+      } else {
+        break;
+      }
     }
   }
 }
